3bb41d4067d1d1f5f8febfd0ef6b84fcf12faf84
Franklin Webber <franklin.webber@gmail.com>
2016-04-01 12:39:06 -0500

    Adds unit test suite to for the default recipe

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -16,5 +16,37 @@ describe 'redis::default' do
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
+
+    it 'execute a package repository update' do
+      expect(chef_run).to run_execute('apt-get update')
+    end
+
+    it 'installs the necessary packages' do
+      expect(chef_run).to install_package('build-essential')
+      expect(chef_run).to install_package('tcl8.5')
+    end
+
+    it 'downloads the redis archive' do
+      expect(chef_run).to create_remote_file('~/redis-2.8.9.tar.gz')
+    end
+
+    it 'unpacks the redis archive' do
+      resource = chef_run.remote_file('~/redis-2.8.9.tar.gz')
+      expect(resource).to notify('execute[tar xzf redis-2.8.9.tar.gz]').to(:run).immediately
+    end
+
+    it 'makes redis and installs it' do
+      resource = chef_run.execute('tar xzf redis-2.8.9.tar.gz')
+      expect(resource).to notify('execute[make && make install]').to(:run).immediately
+    end
+
+    it 'installs redis server' do
+      resource = chef_run.execute('make && make install')
+      expect(resource).to notify('execute[echo -n | ./install_server.sh]').to(:run).immediately
+    end
+
+    it 'starts the redis service' do
+      expect(chef_run).to start_service('redis_6379')
+    end
   end
 end
